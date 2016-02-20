0c18875d9d45cd114e80e3d78566f22f34c82240
Jamie King <king0120@gmail.com>
2016-02-20 10:39:55 -0500

    Working ui-router

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -13,6 +13,12 @@
         templateUrl: 'app/main/main.html',
         controller: 'MainController',
         controllerAs: 'main'
+      })
+      .state('d3', {
+        url: '/d3',
+        templateUrl: 'app/d3/d3.html',
+        controller: 'd3Controller',
+        controllerAs: 'd3'
       });
 
     $urlRouterProvider.otherwise('/');
