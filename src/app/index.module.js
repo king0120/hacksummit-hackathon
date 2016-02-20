cdc797d0b04d4e6e635da7a8692dd014a80f5e58
Koding <king0120@king0120.koding.io>
2016-02-20 08:22:47 -0600

    Initial commit

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('hackathon', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ui.bootstrap', 'toastr']);
+
+})();
