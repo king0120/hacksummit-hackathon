e112389aa66f215cca9f7a0b0c20194e17570315
Jamie King <king0120@gmail.com>
2016-02-20 14:38:44 -0500

    New working map with id of state name

--- src/app/d3/topojson.js
+++ src/app/d3/topojson.js
@@ -1,4 +1,8 @@
 (function(){
+  function stateName(){
+    console.log('hello');
+  };
+
   console.log('hello from topojson');
 
 
@@ -13,100 +17,37 @@
 
   var path = d3.geo.path().projection(projection);
 
-  d3.json('assets/usCounties.json', function(err, us){
-    if (err) return console.error(err);
-    console.log(us);
-
-
-    // //draws the US outline
-    // svg.insert('path', '.graticule')
-    //    .datum(topojson.feature(us, us.objects.land))
-    //    .attr('class', 'land')
-    //    .attr('d', path);
-
-});
-  //   //draws the borders of the districts
-  //   svg.insert('path', '.graticule')
-  //      .datum(topojson.mesh(us, us.objects.counties, function(a, b){return a!==b && !(a.id/1000 ^ b.id/1000);}))
-  //      .attr('class', 'county-boundary')
-  //      .attr('d', path);
-
-  //   //draws each district and gives it a class of district
-  //    svg.selectAll("path")
-  //      .data(topojson.feature(us, us.objects.counties).features)
-  //      .enter().append("path")
-  //      .attr('class', 'district')
-  //      .attr('d', path);
-
-  //   //draws state lines
-  //   svg.insert('path', '.graticule')
-  //     .datum(topojson.mesh(us, us.objects.states, function(a, b){ return a !== b;}))
-  //     .attr('class', 'state-boundry')
-  //     .attr('d', path);
-
-
-  //   svg.selectAll("path")
-  //      .data(topojson.feature(us, us.objects.states).features)
-  //      .enter().append("path")
-  //      .attr('class', 'state')
-  //      .attr('d', path);
-
-  //   //transition for click zoom
-  //   svg.transition().duration(1000)
-  //      .call(zoom)
-  //      .call(zoom.event);
-  // });
-
   d3.json('assets/newmap.json', function(err, us){
     if (err) return console.error(err);
-    console.log(us.features[0]);
 
 
     //draws the US outline
-    console.log(topojson.feature(us, us.features[0]));
     for (i=0; i < us.features.length; i++){
+      var stateName = us.features[i].properties.NAME;
       svg.insert('path', '.graticule')
        .datum( us.features[i])
        .attr('class', 'state')
-       .attr('id', us.features[i].properties.NAME)
-       .attr('d', path);
-
-      console.log(us.features[i].properties.NAME);
+       .attr('id', stateName)
+       .attr('d', path)
+       .on({
+        click: function(){
+          console.log($(this).attr('id'));
+        }
+       });
 
     }
-
-
-  //   //draws the borders of the districts
-  //   svg.insert('path', '.graticule')
-  //      .datum(topojson.mesh(us, us.objects.counties, function(a, b){return a!==b && !(a.id/1000 ^ b.id/1000);}))
-  //      .attr('class', 'county-boundary')
-  //      .attr('d', path);
-
-  //   //draws each district and gives it a class of district
-  //    svg.selectAll("path")
-  //      .data(topojson.feature(us, us.objects.counties).features)
-  //      .enter().append("path")
-  //      .attr('class', 'district')
-  //      .attr('d', path);
 
     //draws state lines
     svg.insert('path', '.graticule')
       .datum(topojson.mesh(us, us.features, function(a, b){ return a !== b;}))
       .attr('class', 'state-boundry')
+
       .attr('d', path);
 
 
-  //   svg.selectAll("path")
-  //      .data(topojson.feature(us, us.objects.states).features)
-  //      .enter().append("path")
-  //      .attr('class', 'state')
-  //      .attr('d', path);
-
-  //   //transition for click zoom
-  //   svg.transition().duration(1000)
-  //      .call(zoom)
-  //      .call(zoom.event);
   });
+
+
 
 })();
 
